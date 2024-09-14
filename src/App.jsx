17733aa988899c48a2325cd65ac2ee3dc4818dfb
unknown <rnbrosca2003@gmail.com>
2024-09-14 19:52:28 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import CustomPage from "./components/CustomPage";
+
+
+function App() {
+  return (
+    <div>
+        <CustomPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CustomPage.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import gsap from 'gsap';
+import { useEffect } from 'react';
+
+function CustomePage() {
+
+  useEffect(() => {
+    gsap.to('.container',{opacity:1, delay:3, y:20, duration:2})
+    gsap.to('.text',{opacity:1, delay:3, y:-250, duration:1.5})
+  })
+  
+  return (
+    
+    <div class="container">
+      <div class="glass">
+        <div class="shine"></div>
+      </div>
+      <div class="thorns">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div class="leaves">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div class="petals">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div class="deadPetals">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+  </div>
+  )
+}
+
+export default CustomePage
